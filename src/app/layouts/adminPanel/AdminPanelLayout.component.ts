4ca523e4b66a5b7abd613769d17543b588f59e30
jasminasevic <jasminasevic@yahoo.com>
2021-05-27 19:25:09 +0200

    Migrate from angular 11 to angular 12

--- src/app/layouts/adminPanel/AdminPanelLayout.component.ts
+++ src/app/layouts/adminPanel/AdminPanelLayout.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-panel',
-  styles: [':host /deep/ .mat-sidenav-content {padding: 0;} .mat-sidenav-container {z-index: 1000}'],
+  styles: [':host ::ng-deep .mat-sidenav-content {padding: 0;} .mat-sidenav-container {z-index: 1000}'],
   templateUrl: './AdminPanelLayout.component.html'
 })
 export class AdminPanelLayoutComponent {}
--- src/app/layouts/adminTheatrePanel/adminTheatrePanel.component.ts
+++ src/app/layouts/adminTheatrePanel/adminTheatrePanel.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'theatre-admin-panel',
-  styles: [':host /deep/ .mat-sidenav-content {padding: 0;} .mat-sidenav-container {z-index: 1000}'],
+  styles: [':host ::ng-deep .mat-sidenav-content {padding: 0;} .mat-sidenav-container {z-index: 1000}'],
   templateUrl: './adminTheatrePanel.component.html',
 })
 export class AdminTheatreLayoutPanelComponent {
